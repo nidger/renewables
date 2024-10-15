7ef6e42b4defecadf53718714ec3f105f8ab2f0b
nidger <nprior70@yahoo.com>
2024-10-15 11:15:47 +0100

    added spacing variables

--- src/components/Greeting.jsx
+++ src/components/Greeting.jsx
@@ -8,7 +8,7 @@ export default function Greeting({messages}) {
 
   return (
     <div>
-      <h3>{greeting}! Thank you for visiting!</h3>
+      <p>{greeting}! Thank you for visiting!</p>
       <button onClick={() => setGreeting(randomMessage())}>
         New Greeting
       </button>
